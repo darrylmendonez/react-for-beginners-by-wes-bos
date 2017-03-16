4a5832a44637fa9caadf78cca32a0df8c7200317
Darryl Mendonez <darryl.mendonez@nucleuscentral.com>
2017-03-16 12:22:20 -0400

    got toggable tabs to work by using ui.bootstrap

--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { BrowserRouter, Match, Miss } from 'react-router';
+
 import './css/style.css';
 import App from './components/App';
 
 import StorePicker from './components/StorePicker';
+
+const Root = () => {
+  return (
+    <BrowserRouter>
+      <Match exactly pattern="/" component={StorePicker} />
+    <Match exactly pattern="/store/:storeId" component={App} />
+    </BrowserRouter>
+  )
+}
 
 render(<App/>, document.querySelector('#main'));
