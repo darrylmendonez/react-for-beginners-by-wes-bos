988d2d1a503fd1a7cff0e508ffe57940401893f9
Darryl Mendonez <darryl.mendonez@nucleuscentral.com>
2017-03-20 09:33:33 -0400

    finished lesson 18 on persisting our state with firebase

---
+++ catch-of-the-day/src/base.js
@@ -0,0 +1,9 @@
+import Rebase from 're-base';
+
+const base = Rebase.createClass({
+  apiKey: "AIzaSyC6gEHTwYhEgFjEEtK_zIA9tUc3QL8Lzrs",
+    authDomain: "catch-of-the-day-darryl-m.firebaseapp.com",
+    databaseURL: "https://catch-of-the-day-darryl-m.firebaseio.com",
+});
+
+export default base;
